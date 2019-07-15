4e6d1b25168058ac73b8341b25a058e6935d2804
svlentink <sv@lent.ink>
2019-07-15 09:42:50 +0000

    added print msgs

--- helpers.go
+++ helpers.go
@@ -3,6 +3,7 @@ package main
 import (
   "bytes"
   "os"
+  "fmt"
   "encoding/json"
   "net/http"
   "gopkg.in/yaml.v2"
@@ -15,14 +16,17 @@ func loadconfig() configfile {
   fileloc := os.Getenv("MANAGEIQ_CONFIGFILE")
   yamlfile, err := ioutil.ReadFile(fileloc)
   if err != nil {
+    fmt.Println("Loading config failed, please supply a valid MANAGEIQ_CONFIGFILE. %T",err)
     panic(err)
   }
 
   var conf configfile
   err2 := yaml.Unmarshal(yamlfile, &conf)
   if err2 != nil {
+    fmt.Println("Failed parsing MANAGEIQ_CONFIGFILE. %T", err2)
     panic(err2)
   }
+  fmt.Println("Loaded config. %T", conf)
   return conf
 }
 
@@ -35,6 +39,7 @@ func apicall(path string, method string, body interface{} ) (map[string]interfac
   var uri_base string = "https://" + conf.api_hostname + "/api/"
   var username string = os.Getenv("MANAGEIQ_USERNAME")
   var password string = os.Getenv("MANAGEIQ_PASSWORD")
+  fmt.Println("User %v will do an API call. %v %v",username,method,path)
 
   client := &http.Client{}
   
@@ -48,13 +53,22 @@ func apicall(path string, method string, body interface{} ) (map[string]interfac
   jsonbody, err := json.Marshal(body)
   reqbody := bytes.NewBuffer(jsonbody)
   req, err := http.NewRequest(method, uri, reqbody)
+  if err != nil {
+    fmt.Println("Failed creating NewRequest. %T",err)
+    panic(err)
+  }
   req.SetBasicAuth(username,password)
   resp, err := client.Do(req)
+  if err != nil {
+    fmt.Println("Failed doing request. %T",err)
+    panic(err)
+  }
   
   var result map[string]interface{}
   json.NewDecoder(resp.Body).Decode(&result)
   //json.Unmarshal(resp.Body,&result)
 
+  fmt.Println("Completed API call. %T",result)
   return result, err
 }
 
